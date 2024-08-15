32a7be3c1902cfb7343ff70a8c201f8dc21d1688
Shashankbhatnagar03 <Shashank.bhatnagar3@gmail.com>
2024-08-15 19:08:01 +0530

    added more details

--- src/constants.js
+++ src/constants.js
@@ -6,7 +6,7 @@ export const dialogueData = {
     Here is my <a href="https://github.com/Shashankbhatnagar03" target="_blank">Github</a>! 
     Let's connect! <a href="https://www.linkedin.com/in/shashank-bhatnagar-874247216/" target="_blank">LinkedIn</a>`,
   "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube . `,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. I also use this time to think about how to solve complex problems I have encountered in competitive coding. You can check out my profiles on <a href="https://codeforces.com/profile/Shashank_bhatnagar03" target="_blank">Codeforces</a> and <a href="https://leetcode.com/u/Shashank_bhatnagar03/" target="_blank">Leetcode</a> for more details on my competitive coding journey. `,
   tv: `That's my TV. I've been watching tech youtubers a lot recently 
    , but I'm also working on some cool projects : <a href="https://github.com/Shashankbhatnagar03/Socia-Buzz-frontend" target="_blank">SociaBuzz</a> , <a href="https://github.com/Shashankbhatnagar03/BlogSpark" target="_blank">BlogSpark</a> , <a href="https://github.com/Shashankbhatnagar03/EasyPay" target="_blank">EasyPay</a> and  <a href="https://github.com/Shashankbhatnagar03/NoteTrackr" target="_blank">NoteTrackr</a> `,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
@@ -14,7 +14,7 @@ export const dialogueData = {
   Contact me at shashank.bhatnagar3@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  Check out my others projects  <a href="https://github.com/Shashankbhatnagar03" target="_blank">Github</a>! `,
   library: `There are a lot of programming books on my shelves.
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
   exit: `If you want to exit Shashank's portfolio, just close the tab.`,
